63885f55d908a44e205d6c7284868ceeaa6bbee0
Sitwat <sitwat.nadeem1@gmail.com>
2018-08-03 18:38:08 -0400

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,52 +1,85 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var allItems = [];
 var connection = mysql.createConnection({
-  host: "localhost",
+ host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+ 
+ port: 3306,
+ user: "root",
 
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "gwbootcamp",
-  database: "bamazon"
+ password: "gwbootcamp",
+ database: "bamazon"
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    Items();
-  });
+   if (err) throw err;
+   console.log("connected as id " + connection.threadId);
+   Items();
+ });
 
-  function Items() {
-    connection.query("SELECT * FROM products", function (err, res) {
-      if (err) { console.error(err); }
-      displayProducts(res);
-    });
-  }
+ function ReturnStock(id)
+ {
+   for (var i = 0; i < allItems.length; i++){
+       if(allItems[i].id == id)
+       {
+         return allItems[i].stock_quantity;
+       }
+   }
 
-  function displayProducts(products){
-      console.log('Products');
-      for (var i = 0; i < products.length; i++){
-        console.log(
-            "Product ID: " + products[i].id +
-            " | Product: " + products[i].product_name +
-            " | Department: " + products[i].department_name +
-            " | Price: " + products[i].price +
-            " | Quantity available:  " + products[i].stock_quantity
-          );
+ }
 
-      }
-  };
+ function Items() {
+   connection.query("SELECT * FROM products", function (err, res) {
+     if (err) { console.error(err); }
+     displayProducts(res);
+   });
+ }
 
-  inquirer.prompt([
+ function displayProducts(products){
+   allItems = products;
+     console.log('Products');
+     for (var i = 0; i < products.length; i++){
+       console.log(
+           "Product ID: " + products[i].id +
+           " | Product: " + products[i].product_name +
+           " | Department: " + products[i].department_name +
+           " | Price: " + products[i].price +
+           " | Quantity available:  " + products[i].stock_quantity
+         );
 
-    {
-      type: "input",
-      name: "id",
-      message: "What is the ID of the item you would like to purchase?"
-    },
-  ])
+     }
+     id();
+ };
+
+ function id(){
+    inquirer.prompt([
+
+   {
+     name: "id",
+     type: "input",
+     message: "What is the ID of the item you would like to purchase?"
+   },
+   {
+     name: "checkQuan",
+     type: "input",
+     message: "How many of that item would you like?"
+   }
+ ]).then(function(inquirerResponse) {
+
+   var stock_quantity = ReturnStock(inquirerResponse.id);
+
+   console.log("inventory left: " + stock_quantity);
+
+   if(parseInt(stock_quantity) < parseInt(inquirerResponse.checkQuan))
+   {
+     console.log("Sorry we have less quantity");
+   }
+   else{
+     console.log("Awesome we have enough inventory");
+   }
+
+ }
+ )
+}
--- schema.sql
+++ schema.sql
@@ -1,12 +1,51 @@
 CREATE DATABASE bamazon;
-
 USE bamazon;
 
 CREATE TABLE products(
-  item_id INT NOT NULL,
+  id INT NOT NULL,
   product_name VARCHAR(45) not NULL,
   department_name VARCHAR(45) not NULL,
   price DECIMAL(10,4) not NULL,
   stock_quantity INT not null,
-  primary key (item_id)
+  primary key (id)
 );
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (1, "Bracelet", "Jewelry", 15.99, 40);
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (2, "Top", "Clothing", 5.99,	23);
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (3, "Iphone", "Electronics", 500.54, 34);
+
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (4,	"USB cable",	"Electronics",	12.65,	78);
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (5,	"Redkin shampoo",	"Hair care",	40.98,	12);
+
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+values (6,	"Gucci Perfume",	"Fragrance",	286.65,	10);
+
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+values (7,	"Guitar",	"Instrument",	50.89,	5);
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (8,	"Wall clock",	"Home décor",	25.67,	16);
+
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (9,	"Coach watch",	"Accessories",	350.98,	20);
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (10,	"MK handbang",	"Accessories",	698.45,	16);
+
+
+
+
+
+
+
